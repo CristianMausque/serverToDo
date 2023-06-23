a58a8c75915df4063fb5ef61afeb360a0f47e17b
Fernando Durban <ferbrizie@gmail.com>
2023-06-23 10:37:34 +0200

    cambiosfer

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,44 +1,43 @@
 const User = require('../models/User.model')
 
-
 const signup = (req, res, next) => {
-  
-  const { name, surname, surname2, username, email, password, birthdate, position, avatar, role, tasks} = req.body
+
+    const { name, surname, surname2, username, email, password, birthdate, position, avatar, role, tasks } = req.body
 
     User
-      .create({ name, surname, surname2, username, email, password, birthdate, position, avatar, role, tasks})
-      .then( () => res.sendStatus(201))
-      .catch(err => next(err))
+        .create({ name, surname, surname2, username, email, password, birthdate, position, avatar, role, tasks })
+        .then(() => res.sendStatus(201))
+        .catch(err => next(err))
 
 }
 
 const login = (req, res, next) => {
 
-    const { email, password } = req.body 
+    const { email, password } = req.body
 
     if (email === '' || password === '') {
-        res.status(400).json({ errorMessages: ["Provide email and password."] }) 
-        return 
+        res.status(400).json({ errorMessages: ["Provide email and password."] })
+        return
     }
 
     User.findOne({ email })
         .then((foundUser) => {
 
-        if (!foundUser) {
-            res.status(401).json({ errorMessages: ["User not found."] })
-            return
-        }
+            if (!foundUser) {
+                res.status(401).json({ errorMessages: ["User not found."] })
+                return
+            }
 
-        if (foundUser.validatePassword(password)) {
-            const authToken = foundUser.signToken()
-            res.json({ authToken: authToken })
-        }
-        else {
-            res.status(401).json({ errorMessages: ["Unable to authenticate the user."] }) 
-        }
+            if (foundUser.validatePassword(password)) {
+                const authToken = foundUser.signToken()
+                res.json({ authToken: authToken })
+            }
+            else {
+                res.status(401).json({ errorMessages: ["Unable to authenticate the user."] })
+            }
 
         })
-        .catch(err => next(err)) 
+        .catch(err => next(err))
 
 }
 
@@ -47,4 +46,4 @@ const verify = (req, res, next) => {
 }
 
 
-module.exports = {signup, login, verify}
+module.exports = { signup, login, verify }
--- models/User.model.js
+++ models/User.model.js
@@ -1,5 +1,9 @@
 const { Schema, model } = require("mongoose")
 const Todo = require("./Todo.model")
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
 
 const userSchema = new Schema(
   {
@@ -8,21 +12,21 @@ const userSchema = new Schema(
       required: [true, "Name is required"],
       trim: true,
       minlength: [2, 'The username must be 2 characters minimum'],
-      maxlength: [15, 'The username must be 15 characters maximum'] 
+      maxlength: [15, 'The username must be 15 characters maximum']
     },
     surname: {
       type: String,
       required: [true, "Username is required"],
       trim: true,
       minlength: [2, 'The username must be 2 characters minimum'],
-      maxlength: [15, 'The username must be 15 characters maximum'] 
+      maxlength: [15, 'The username must be 15 characters maximum']
     },
     surname2: {
       type: String,
       required: [true, "Username is required"],
       trim: true,
       minlength: [2, 'The username must be 2 characters minimum'],
-      maxlength: [15, 'The surname must be 15 characters maximum'] 
+      maxlength: [15, 'The surname must be 15 characters maximum']
     },
     username: {
       type: String,
@@ -31,7 +35,7 @@ const userSchema = new Schema(
       trim: true,
       lowercase: true,
       minlength: [2, 'The username must be 2 characters minimum'],
-      maxlength: [15, 'The username must be 15 characters maximum'] 
+      maxlength: [15, 'The username must be 15 characters maximum']
     },
     email: {
       type: String,
@@ -75,34 +79,34 @@ const userSchema = new Schema(
   }
 )
 
-// userSchema.pre('save', function (next) {
+userSchema.pre('save', function (next) {
 
-//   const saltRounds = 10
-//   const salt = bcrypt.genSaltSync(saltRounds)
-//   const hashedPassword = bcrypt.hashSync(this.password, salt)
-//   this.password = hashedPassword
+  const saltRounds = 10
+  const salt = bcrypt.genSaltSync(saltRounds)
+  const hashedPassword = bcrypt.hashSync(this.password, salt)
+  this.password = hashedPassword
 
-//   next()
-// })
-
-
-// userSchema.methods.signToken = function () {
-//   const { _id, username, email, profileImg } = this
-//   const payload = { _id, username, email, profileImg }
-
-//   const authToken = jwt.sign(
-//     payload,
-//     process.env.TOKEN_SECRET,
-//     { algorithm: 'HS256', expiresIn: "6h" }
-//   )
-
-//   return authToken
-// }
+  next()
+})
 
 
-// userSchema.methods.validatePassword = function (candidatePassword) {
-//   return bcrypt.compareSync(candidatePassword, this.password)
-// }
+userSchema.methods.signToken = function () {
+  const { _id, username, email, profileImg } = this
+  const payload = { _id, username, email, profileImg }
+
+  const authToken = jwt.sign(
+    payload,
+    process.env.TOKEN_SECRET,
+    { algorithm: 'HS256', expiresIn: "6h" }
+  )
+
+  return authToken
+}
+
+
+userSchema.methods.validatePassword = function (candidatePassword) {
+  return bcrypt.compareSync(candidatePassword, this.password)
+}
 
 
 const User = model("User", userSchema)
